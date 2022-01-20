98136f312215c54a43044eb120563a98714db49a
vinhnguyen-22 <vinhnguyenad22@gmail.com>
2022-01-20 20:05:53 +0700

    Coordinates

---
+++ src/summaryofexercises/Coordinates/Coordinates.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package summaryofexercises.Coordinates;
+
+/**
+ *
+ * @author ACER
+ */
+public class Coordinates {
+    private double x;
+    private double y;
+
+    public Coordinates(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Coordinates() {
+    }
+    
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Coordinates{" + "x=" + x + ", y=" + y + '}';
+    }
+}
